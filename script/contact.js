1ac8520abd2557c373212daa2d3e148c8154a617
Narihs <shifshif18@gmail.com>
2019-11-11 12:28:27 +0200

    first commit

---
+++ script/contact.js
@@ -0,0 +1,63 @@
+﻿(function () {
+    "use strict";
+    $(function () {
+
+        $("#btnSubmit").click(function () {
+
+            if ($("#fullName").val() == "") {
+                return;
+            }
+            else if (validateEmail()) {
+                return;
+            }
+            else if (validateDate()) {
+                return false;
+            }
+            else if ($("#inputMessage").val() == "") {
+                return;
+            }
+            else {
+                $("#dialogMessage").modal('show');
+                return false;
+            }
+        });
+
+        $("#modalBtn").click(function () {
+            $("#contactForm")[0].reset();
+        });
+
+    });
+
+    function validateDate() {
+
+        if ($("#inputDate").val() == "") {
+            return true;
+        }
+        else {
+            var toCompare = new Date($("#inputDate").val());
+            var today = new Date();
+            if (toCompare.getTime() < today.getTime()) {
+                alert("Please insert a future date");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function validateEmail() {
+        var regex = /.+@.+\.\w{2,3}(\..{,2})?/
+
+        if ($("#inputEmail").val() == "") {
+            return true;
+        }
+        else {
+            if (regex.test($("#inputEmail").val())) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+})();
+
---
+++ script/site.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    "use strict";
+
+    $(function () {
+
+        $(".imgDiv").on("mouseover", "img", function () {
+            var city = $(this).attr("alt");
+            var img = $(this);
+           
+            $.ajax({
+                
+                type: "GET",
+                url: "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=dda2ae43958bb28f49cc794105bad652&units=metric",
+
+                error: function (err) {
+                    alert("Error: " + err.status + "' " + err.statusText);
+                },
+                success: function (response) {
+
+                    var arr = response;
+                    var temp = parseInt(arr.main.temp);
+                    var weather = arr.weather[0].description;
+
+                    $(img).siblings(".overlay").children(".weatherStat").html($(img).attr("alt") + "<br/><br/>Temp: " + temp + " C <br/> Current Conditions: " + weather);
+                }
+            });
+            
+        });
+
+    });
+})();
